e858678844f0ffc66c2671e5dcc09f342c7e566f
loveroni <44824452+loveroni@users.noreply.github.com>
2018-11-14 11:31:40 +0900

    Add files via upload

---
+++ kandinsky_int.js
@@ -0,0 +1,320 @@
+function setup() {
+  createCanvas(500,509);
+  background(0);
+}
+
+function draw() {
+  //blue
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+    ellipse(217,192,212,212);
+}
+fill(6,40,168);
+ellipse(217,192,212,212);
+
+
+//black
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+    ellipse(194,173,150,150);
+}
+fill(10,10,10);
+ellipse(194,173,150,150);
+
+//green
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+ellipse(355,158,80,80);
+}
+fill(30,83,81,100);
+ellipse(355,158,80,80);
+
+//greenmiddle
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+    ellipse(355,158,25,25);
+}
+fill(52,148,172);
+noStroke();
+ellipse(355,158,25,25);
+fill(0);
+ellipse(355,158,20,20);
+
+//yello
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+    ellipse(48,50,20,20);
+}
+fill(234,210,28);
+ellipse(48,50,20,20);
+
+//pink
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+ellipse(60,101,40,40);
+}
+fill(238,133,200);
+ellipse(60,101,40,40);
+
+//purple
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+ellipse(263,108,60,60);
+}
+fill(186,112,207,120);
+ellipse(263,108,60,60);
+
+//yellow
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+ellipse(361,90,30,30);}
+fill(251,221,91);
+ellipse(361,90,30,30);
+
+//orange
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(410,115,20,20);}
+fill(250,148,7);
+ellipse(410,115,20,20);
+
+//red
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(437,75,15,15);}
+fill(223,35,54);
+ellipse(437,75,15,15);
+
+//green
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(266,214,40,40);}
+fill(140,212,158,150);
+ellipse(266,214,40,40);
+
+//bluesmall
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(237,227,20,20);}
+fill(7,47,173,150);
+ellipse(237,227,20,20);
+
+//orange
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(285,259,70,70);}
+fill(213,142,32,150);
+ellipse(285,259,70,70);
+
+//gold
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(250,258,40,40);}
+fill(190,154,28,150);
+ellipse(250,258,40,40);
+
+//pinksmall
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(309,273,25,25);}
+fill(253,101,156,100);
+ellipse(309,273,25,25);
+
+//purple
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(201,298,40,40);}
+fill(149,161,213,100);
+ellipse(201,298,40,40);
+
+//leftpurple
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(66,287,50,50);}
+fill(149,161,213,100);
+ellipse(66,287,50,50);
+fill(0);
+ellipse(75,275,15,15);
+
+//gold
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(125,292,40,40);}
+fill(210,159,40,100);
+ellipse(125,292,40,40);
+
+//brown
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(148,315,40,40);}
+fill(186,77,39,100);
+ellipse(148,315,40,40);
+
+//yellow
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(98,336,40,40);}
+fill(250,224,70,100);
+ellipse(98,336,40,40);
+
+//red
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(118,339,40,40);}
+fill(198,23,49,100);
+ellipse(118,339,40,40);
+
+//smallred
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(154,287,5,5);}
+fill(198,23,49,100);
+ellipse(154,287,5,5);
+
+//pink
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(138,341,40,40);}
+fill(228,106,158,100);
+ellipse(138,341,40,40);
+
+//blue
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(285,316,50,50);}
+fill(53,161,210,100);
+ellipse(285,316,50,50);
+
+//lightpurple
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(413,361,90,90);}
+fill(183,187,216,100);
+ellipse(413,361,90,90);
+
+//lightbrown
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(359,339,30,30);}
+fill(196,149,103,100);
+ellipse(359,339,30,30);
+
+//smallblue
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(446,259,15,15);}
+fill(4,56,205,100);
+ellipse(446,259,15,15);
+
+//smallmint
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(389,348,30,30);}
+fill(114,204,214,100);
+ellipse(389,348,30,30);
+
+//smallred
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(418,323,10,10);}
+fill(193,17,25);
+ellipse(418,323,10,10);
+
+//smallblack
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(428,323,7,7);}
+fill(0);
+ellipse(428,323,7,7);
+//smallgold
+if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(464,358,15,15)}
+fill(189,155,40);
+ellipse(464,358,15,15);
+
+//smallorange
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(248,386,20,20);}
+fill(237,159,21);
+ellipse(248,386,20,20);
+
+//smallpink
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(70,393,10,10);}
+fill(237,129,187);
+ellipse(70,393,10,10);
+
+//smallblue
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(96,415,20,20);}
+fill(37,143,204);
+ellipse(96,415,20,20);
+
+//smallblack
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(62,432,10,10);}
+fill(0);
+ellipse(62,432,10,10);
+
+//smallyellow
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(397,415,10,10);}
+fill(233,208,21);
+ellipse(397,415,10,10);
+
+//smallred
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(386,446,15,15);}
+fill(255,2,2);
+ellipse(386,446,15,15);
+
+//smallblue
+  if (mouseIsPressed) {
+    fill(random(0,255),random(0,255),random(0,255),100);
+
+ellipse(429,464,12,12);}
+fill(27,115,172);
+ellipse(429,464,12,12);
+
+}
+
+
+function keyPressed(){
+  background(random(0,255), random(0,255), random(0,255));
+}
